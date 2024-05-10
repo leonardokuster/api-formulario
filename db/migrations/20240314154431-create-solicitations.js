e76a18788881426a9ba0485e8a0c9a6af01ee105
Leonardo Cristiano Kuster <71235282+leonardokuster@users.noreply.github.com>
2024-05-10 14:04:10 -0300

    Update 20240314154431-create-solicitations.js

--- db/migrations/20240314154431-create-solicitations.js
+++ db/migrations/20240314154431-create-solicitations.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Solicitations', {
+    await queryInterface.createTable('solicitacaoContato', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -35,6 +35,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Solicitations');
+    await queryInterface.dropTable('solicitacaoContato');
   }
 };
