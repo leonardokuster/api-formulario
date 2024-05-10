0024fb62426296f9a652b24d794c9c9813c350db
Leonardo Cristiano Kuster <71235282+leonardokuster@users.noreply.github.com>
2024-05-10 14:22:07 -0300

    Update and rename solicitations.js to contatos.js

--- db/models/contatos.js
+++ db/models/contatos.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class solicitacaoContato extends Model {
+  class contatos extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  solicitacaoContato.init({
+  contatos.init({
     subject: DataTypes.STRING,
     fullname: DataTypes.STRING,
     email: DataTypes.STRING,
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     content: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'solicitacaoContato',
+    modelName: 'contatos',
   });
-  return solicitacaoContato;
+  return contatos;
 };
