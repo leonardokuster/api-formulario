a57591aeb8355868dc54e426ebe7552b34452b48
Leonardo Cristiano Kuster <l.kusterr@gmail.com>
2024-05-17 08:40:14 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
-const sequelize = require('sequelize');
 
 app.use(express.json());
 
@@ -18,10 +17,6 @@ app.use(cors());
 const contatos = require("../controllers/contatos");
 app.use('/contato', contatos);
 
-sequelize.sync().then(() => {
-    app.listen(8080, () => {
-        console.log("Servidor iniciado na porta 8080: http://localhost:8080");
-    });
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080");
 });
-
-
